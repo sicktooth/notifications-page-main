b0fe5c38ff3f1c5ea284fa66e69cd7da117a8ded
sicktooth <sosaronungwe@gmail.com>
2024-06-13 00:06:29 +0100

    updated

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const clear = $(".clearRead"),
     clearRead = $(".redDot"),
-    number = $("var");
+    number = $("var"),
+    newNotify = $(".removeBg");
 
-
+$("article").css("cursor", "pointer")
 clear.on("click", function () {
     clearRead.removeClass("redDot");
     number.text("0");
+    newNotify.removeClass("bg-newNotifyBgColor");
 });  /**
  * Event handler for the clear button click event.
  * Removes the 'edDot' class from the 'clearRead' element.
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'groupHoverColor': 'hsl(219, 14%, 63%)'
+        'groupHoverColor': 'hsl(219, 14%, 63%)',
+        'PMHoverColor': 'hsl(211, 68%, 94%)',
+        'gNameColor': 'hsl(219, 85%, 26%)',
+        'newNotifyBgColor': 'hsl(210, 60%, 98%)'
       }
     },
   },
   variants: {
     extend: {
+      backgroundColor: ['hover'],
       textColor: ['hover']
     }
   },
