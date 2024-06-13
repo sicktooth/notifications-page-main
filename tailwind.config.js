1d1b1d3e038a6c1dad4ce966153c58336da1d7d0
sicktooth <sosaronungwe@gmail.com>
2024-06-13 19:58:18 +0100

    working on responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
         'gNameColor': 'hsl(219, 85%, 26%)',
         'newNotifyBgColor': 'hsl(210, 60%, 98%)',
         'postColor': 'hsl(0, 1%, 36%)'
+      },
+      screens: {
+        '3sm': '319px',
+        '2sm': '374px'
       }
     },
   },
