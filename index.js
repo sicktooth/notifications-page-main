a9ebc648de599947353a4dc7e0b24f42782ee2fd
sicktooth <sosaronungwe@gmail.com>
2024-06-16 06:20:48 +0100

    fixed it

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const clear = $(".clearRead"),
-    after = $(".redDot"),
     number = $("var"),
     newNotify = $(".removeBg");
 
 clear.on("click", function () {
-    if (after) {
-       const style = window.getComputedStyle(after, ':after');
-       style.css('display', 'none');
-    }
+    $('style').append(".redDot::after{ display:none}")
     number.text("0");
     newNotify.removeClass("bg-newNotifyBgColor");
 });  /**
